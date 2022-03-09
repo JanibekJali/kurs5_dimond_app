5121a0a9ad56a2476ef36567cb33a48cf22de134
Janibek <janibekjali@gmail.com>
2022-03-10 00:44:33 +0600

    sabak3

---
+++ lib/constants/body_style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class BodyStyle {
+  static const textStyle = TextStyle(
+    fontFamily: 'Pacifico',
+    fontSize: 35.0,
+    color: Color(0xfff3423b),
+  );
+}
